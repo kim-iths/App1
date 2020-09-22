1e7e3f43825884c7688f4dab58058fee001dc0a9
kim-iths <kim.hellman@iths.se>
2020-09-22 14:32:40 +0200

    moved some code to functions, added reset function, added game over toast

--- app/src/main/java/com/example/app1/MainActivity.kt
+++ app/src/main/java/com/example/app1/MainActivity.kt
@@ -18,57 +18,64 @@ class MainActivity : AppCompatActivity() {
     lateinit var adapter: ButtonAdapter
     var amountToAdd = 3
     var random = 0
-    var round = 1
+    var round = 1;
+
     lateinit var buttonList: MutableList<ColorButton>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         buttonList = mutableListOf()
         buttonList.add(ColorButton(R.color.colorPrimaryDark))
 
         grid = findViewById(R.id.grid)
+        val resetButton = findViewById<Button>(R.id.buttonReset)
         adapter = ButtonAdapter(this, buttonList)
         grid.adapter = adapter
 
-        //Log.e("!!!", "${buttonList.size}")
+        resetButton.setOnClickListener {
+            reset()
+        }
 
         grid.setOnItemClickListener { adapterView, view, i, l ->
 
-            Log.e("!!!", "$i")
-            ++grid.numColumns
-            for(j in 1..amountToAdd){
-                buttonList.add(ColorButton(R.color.colorPrimary))
+            if(i == random){
+                Log.e("!!!", "$i")
+                ++grid.numColumns
+                round += 1
+                nextRound(round)
+            }else{
+                Toast.makeText(this, getString(R.string.toast_game_over), Toast.LENGTH_SHORT).show()
+                reset()
             }
-            //nextRound((amountToAdd-1)*(amountToAdd-1))
-            //buttonList[random].color = R.color.colorPrimaryDark
+        }
+    }
 
-            round += 1
-            buttonList[random].color = R.color.colorPrimary
-            nextRound(round)
-            adapter.notifyDataSetChanged()
-            amountToAdd += 2
+    fun nextRound(count: Int) {
+        buttonList[random].color = R.color.colorPrimary
+
+        for (i in 1..amountToAdd) {
+            buttonList.add(ColorButton(R.color.colorPrimary))
         }
 
-    }
-
-    fun nextRound(count: Int){
-        random = Random.nextInt(0,(count) * (count))
+        random = Random.nextInt(0, (count) * (count))
         Log.e("!!!", "count: $count, random: $random")
         buttonList[random].color = R.color.colorPrimaryDark
+        adapter.notifyDataSetChanged()
+        amountToAdd += 2
+    }
 
-        //for(i in 0..count-1){
-
-
-            //buttonList.add(ColorButton(R.color.colorPrimary))
-            //grid.getChildAt(i).setBackgroundColor(resources.getColor(R.color.colorPrimaryDark))
-        //}
-
-        //grid.getChildAt(random).setBackgroundColor(resources.getColor(R.color.colorPrimaryDark))
-
-        //grid.getChildAt(random).setOnClickListener {
-            //makeButtons(count+2)
-        //}
+    fun reset(){
+        grid.numColumns = 1
+        round = 1
+        amountToAdd = 3
+        random = 0
+        for(i in 0 until buttonList.size-1)buttonList.removeLast()
+        buttonList[0].color = R.color.colorPrimaryDark
     }
 }
+
+
+//TODO("In harder difficulties, make the correct button change place after a certain amount of time")
